b5b9bff1d29646017fa8ef0a436377252d592093
Jhonny Lima <jhonny.lima@funcionalcorp.com.br>
2024-04-03 19:04:58 -0300

    feat: add MainNavigation component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,12 +1,17 @@
 import { Logo } from './Logo'
+import { MainNavigation } from './MainNavigation'
 import { SearchBar } from './SearchBar'
 
 export function Sidebar() {
   return (
     <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
-      <Logo />
+      <div className="mx-2 flex flex-col gap-6">
+        <Logo />
 
-      <SearchBar />
+        <SearchBar />
+      </div>
+
+      <MainNavigation />
     </aside>
   )
 }
