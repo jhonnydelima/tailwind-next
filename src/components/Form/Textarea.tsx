2d440d610a6572449bc4345a82fa12c18bcb5f72
Jhonny Lima <jhonny.lima@funcionalcorp.com.br>
2024-04-05 15:31:47 -0300

    feat: add Textarea component and finish page structure

---
+++ src/components/Form/Textarea.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { ComponentProps } from 'react'
+
+interface TextareaProps extends ComponentProps<'textarea'> {}
+
+export function Textarea(props: TextareaProps) {
+  return (
+    <textarea
+      className="py2 min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    />
+  )
+}
